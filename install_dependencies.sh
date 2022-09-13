d1cd2d0618beed0d39ba2e97ea37b11ad7e6ffc6
Vinay Talla <98005486+VinayTalla20@users.noreply.github.com>
2022-09-13 18:05:44 +0530

    Update install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt-get update
 cd /home/ubuntu
-suod apt-get install maven -y
+sudo apt-get install maven -y
 sudo apt-get install -y default-jre
 sudo apt-get install -y default-jdk
 wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.65/bin/apache-tomcat-9.0.65.tar.gz
